f933096a79b0a9dae4676934f10d0046e9c8ec72
eliasedwin7 <eliasedwin7@gmail.com>
2025-02-14 05:08:58 +0000

    1st commit template

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="console_test_runner",
+    version="1.0.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "pytest",
+    ],
+    entry_points={
+        "console_scripts": ["console-test-runner=main:main"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
